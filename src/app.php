489be17d7cac00f34f5f9853d2d7e2586891ed7d
ouerghi <ouerghimahdi@gmail.com>
2019-03-04 17:17:10 +0100

    load framework after config kernel

---
+++ src/app.php
@@ -0,0 +1,10 @@
+<?php
+
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => 'Calendar\Controller\LeapYearController::index',
+]));
+return $routes;
